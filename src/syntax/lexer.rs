7fd4a80d70bcad1647386d21cac2c867557ba79d
vulns-are-features-too <57666296+vulns-are-features-too@users.noreply.github.com>
2026-08-02 15:39:55 +0700

    feat(lexer): lex whitespaces & newlines

--- src/syntax/lexer.rs
+++ src/syntax/lexer.rs
@@ -17,12 +17,42 @@ impl LexingError {
 pub fn lex(s: &str) -> Result<Vec<Token<'_>>, LexingError>
 {
     let mut chars = s.chars().enumerate().peekable();
-    let line_num = 0;
+    let mut line_num = 0;
     let mut col_num = 0;
     let mut tokens = vec![];
 
     while let Some((_, c)) = chars.peek() {
         match c {
+            // whitespace
+            ' ' | '\t' => {
+                tokens.push(Token::WhiteSpace);
+                col_num += 1;
+                chars.next();
+                while let Some(&(_, c2)) = chars.peek()
+                    && matches!(c2, ' ' | '\t')
+                {
+                    col_num += 1;
+                    chars.next();
+                }
+            }
+            '\n' => {
+                tokens.push(Token::NewLine);
+                col_num = 0;
+                line_num += 1;
+                chars.next();
+            }
+            '\r' => {
+                tokens.push(Token::NewLine);
+                col_num = 0;
+                line_num += 1;
+                chars.next();
+                if let Some(&(_, c2)) = chars.peek()
+                    && c2 == '\n'
+                {
+                    chars.next();
+                }
+            }
+
             // single char
             ';' => {
                 tokens.push(Token::SemiColon);
@@ -90,10 +120,76 @@ mod tests {
     #[case("}", Token::RightBrace)]
     #[case(";", Token::SemiColon)]
     #[case(":", Token::Colon)]
+    // newlines
+    #[case("\n", Token::NewLine)]
+    #[case("\r\n", Token::NewLine)]
+    #[case("\r", Token::NewLine)]
+    // whitespace
+    #[case(" ", Token::WhiteSpace)]
+    #[case("\t", Token::WhiteSpace)]
+    #[case(" \t ", Token::WhiteSpace)]
     fn single_token(#[case] s: &str, #[case] expected: Token) {
         let tokens = lex(s).expect("failed to lex");
         assert_eq!(expected, tokens[0]);
         assert_eq!(2, tokens.len());
         assert_eq!(Token::Eof, tokens[1]);
+    }
+
+    #[rstest]
+    // only newlines
+    #[case("\n", 1)]
+    #[case("\r", 1)]
+    #[case("\n\n", 2)]
+    #[case("\n\r", 2)]
+    #[case("\r\n", 1)]
+    #[case("\r\r", 2)]
+    #[case("\r\n\n", 2)]
+    #[case("\n\r\n", 2)]
+    #[case("\n\n\r", 3)]
+    #[case("\n\r\r", 3)]
+    #[case("\r\n\r", 2)]
+    #[case("\r\r\n", 2)]
+    #[case("\r\n\r\n", 2)]
+    // with extras
+    #[case(" \n ", 1)]
+    #[case(" \r ", 1)]
+    #[case(" \n\n ", 2)]
+    #[case(" \n\r ", 2)]
+    #[case(" \r\n ", 1)]
+    #[case(" \r\r ", 2)]
+    #[case("\r \n", 2)]
+    #[case("\n \r", 2)]
+    #[case(" \r \n ", 2)]
+    fn count_new_lines(#[case] s: &str, #[case] expected: usize) {
+        let tokens = lex(s).expect("failed to lex");
+        let count = tokens
+            .iter()
+            .filter(|x| matches!(x, Token::NewLine))
+            .count();
+        assert_eq!(expected, count);
+    }
+
+    #[rstest]
+    #[case(" ", 1)]
+    #[case("  ", 1)]
+    #[case("\t", 1)]
+    #[case("\t\t", 1)]
+    #[case(" \t", 1)]
+    #[case("\t ", 1)]
+    #[case(" \t ", 1)]
+    #[case("\t \t", 1)]
+    #[case(" \n ", 2)]
+    #[case("\t\n\t", 2)]
+    #[case(" ; ", 2)]
+    #[case(" ;\t", 2)]
+    #[case("\t; ", 2)]
+    #[case("\t;\t", 2)]
+    fn count_whitespace(#[case] s: &str, #[case] expected: usize) {
+        let tokens = lex(s).expect("failed to lex");
+        let count = tokens
+            .iter()
+            .filter(|x| matches!(x, Token::WhiteSpace))
+            .count();
+        assert_eq!(expected, count);
     }
 }
