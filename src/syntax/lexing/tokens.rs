0222093f77c0652105ff600d19faef0513f5abda
vulns-are-features-too <57666296+vulns-are-features-too@users.noreply.github.com>
2026-08-05 18:02:00 +0700

    chore: fix token doc comments

--- src/syntax/lexing/tokens.rs
+++ src/syntax/lexing/tokens.rs
@@ -9,32 +9,32 @@ pub enum Token<'a> {
     True(usize),
     False(usize),
     Decimal(&'a str),
-    Exponential(&'a str),
     /// beN/bEN where b=base, N=exponent
-    BigExpDec(&'a str),
+    Exponential(&'a str),
     /// [0-9]...e...n
-    BigInt(&'a str),
+    BigExpDec(&'a str),
     /// ends with n
-    Binary(&'a str),
+    BigInt(&'a str),
     /// starts with 0b/0B
-    BigBin(&'a str),
+    Binary(&'a str),
     /// 0b...n
-    ExpBin(&'a str),
+    BigBin(&'a str),
     /// 0b...e...
-    BigExpBin(&'a str),
+    ExpBin(&'a str),
     /// 0b...e...n
-    Octal(&'a str),
+    BigExpBin(&'a str),
     /// starts with 0/0o/0O
-    BigOct(&'a str),
+    Octal(&'a str),
     /// 0o...n
-    ExpOct(&'a str),
+    BigOct(&'a str),
     /// 0o...e...
-    BigExpOct(&'a str),
+    ExpOct(&'a str),
     /// 0o...e...n
-    Hexadecimal(&'a str),
+    BigExpOct(&'a str),
     /// starts with 0x/0X
-    BigHex(&'a str),
+    Hexadecimal(&'a str),
     /// 0x...n
+    BigHex(&'a str),
     String(&'a str),
     Regex(&'a str),
     Template(&'a str),
